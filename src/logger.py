8e6b04dfc4e9af7d5aaa8edf90b823e9fad43507
BhardwajG572 <rudrakumargaurav572@gmail.com>
2023-09-30 19:35:35 +0530

    logging and exceptions

---
+++ src/logger.py
@@ -0,0 +1,34 @@
+import logging 
+
+import os 
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True) # even though the file , keep on assigning the file.
+
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+
+FORMAT = '[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s'
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format=FORMAT,
+    level=logging.INFO
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
