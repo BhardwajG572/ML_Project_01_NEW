596c1df9c6b8b87d8cd8a9af3b730a593a75b707
BhardwajG572 <rudrakumargaurav572@gmail.com>
2023-10-01 22:12:04 +0530

    added EDA,etc

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,14 @@
 import sys
-import logging
+sys.path.append("C:/Users/.m/Desktop/End_to_End_ML_New/ML_Project/src")
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # execution info : on which file exception has occured, on which line exceptions occured etc .
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name[{0}] line number [{1}] error message[{2}] ".format()
     file_name,exc_tb.tb_lineno,str(error)
+
+
     return error_message
 
 
@@ -16,4 +19,20 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    
+
+# for exception testing : 
+
+''' 
+
+if __name__=="__main__" :
+
+    try:
+        a=1/0
+
+    except Exception as e: 
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
+        
+
+'''
 
